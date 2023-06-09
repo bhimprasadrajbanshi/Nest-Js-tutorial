3a557f01e86ad51222040ca510cc4b9e6f1f68b9
bhimprasadrajbanshi <rajbanshibhimprasad123@gmail.com>
2023-06-09 22:01:47 +0545

    delete some file

--- README.md
+++
@@ -1,2 +0,0 @@
-# Nest-Js Root module with Sub module
-<img src="https://docs.nestjs.com/assets/Modules_1.png" alt="Girl in a jacket" width="700" height="400">
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RootModule } from './root.module';
-
-
-async function bootstrap() {
-  const app = await NestFactory.create(RootModule);
-  await app.listen(3000);
-}
-bootstrap();
