07a61ebfdd4a06830aa5478b0e1334f27c03534e
bhimprasadrajbanshi <rajbanshibhimprasad123@gmail.com>
2023-06-20 21:55:49 +0545

    delete the studentifo file

--- studentinfo/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { StudentModule } from './student/student.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(StudentModule);
-  await app.listen(3000);
-}
-bootstrap();
