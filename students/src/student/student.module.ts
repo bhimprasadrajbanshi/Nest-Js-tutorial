dba1ac5a34906776eef827f368b08eb24e52d21a
bhimprasadrajbanshi <rajbanshibhimprasad123@gmail.com>
2023-06-25 23:28:50 +0545

    oh sorry wrong project to be add

--- students/src/student/student.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StudentController } from './student.controller';
-
-@Module({
-  controllers: [StudentController]
-})
-export class StudentModule {}
